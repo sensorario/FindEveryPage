9017b9f8255eb94809f3c4510f65948fea3123b9
Simone Gentili <sensorario@gmail.com>
2022-09-28 09:49:01 +0200

    Update FindEveryPageClass.php

--- src/FindEveryPageClass.php
+++ src/FindEveryPageClass.php
@@ -1,25 +1,34 @@
 <?php
+
 namespace Kryuko\FindEveryPage;
+
 use Symfony\Component\DomCrawler\Crawler;
 
-class FindEveryPageClass {
-    function __construct() {
-        $this->baseUrl = '';
-        $this->urlsArray = [];
-        $this->sleepTime = 1;
-        $this->enableLog = false;
-        $this->logFileName = 'final_log.json';
-    }
+final class FindEveryPageClass
+{
+    private string $baseUrl = '';
+    
+    private array $urlsArray = [];
+    
+    private int $sleepTime = 1;
+    
+    private bool $enableLog = false;
+    
+    private $logFileName = 'final_log.json';
+    
+    public function __construct() { }
 
-    public function setBaseData($data) {
+    public function setBaseData($data)
+    {
         $this->baseUrl = (($data['baseUrl']) ? $data['baseUrl'] : $this->baseUrl);
         $this->sleepTime = ((@$data['sleepTime']) ? @$data['sleepTime'] : $this->sleepTime);
         $this->enableLog = ((@$data['enableLog']) ? @$data['enableLog'] : $this->enableLog);
         $this->logFileName = (@($data['logFileName']) ?@ $data['logFileName'] : $this->logFileName);
     }
 
-    public function automate() {
-        if (strlen($this->baseUrl)<4) {
+    public function automate()
+    {
+        if (strlen($this->baseUrl) < 4) {
             $this->printlog('>>> BASE URL NOT FOUND!!!', '!!!ERROR!!!');
             exit();
         }
@@ -41,8 +50,8 @@ class FindEveryPageClass {
         return $this->urlsArray;
     }
 
-    public function updateArrayWithCurrentPageUrls($url) {
-
+    public function updateArrayWithCurrentPageUrls($url)
+    {
         $this->printlog('>>> DOWNLOADING PAGE > '.$url, '___START_CRAWLING___');
 
         $page = file_get_contents($url);
@@ -73,43 +82,49 @@ class FindEveryPageClass {
         sleep($this->sleepTime);
     }
 
-    public function hasCrawlingDoneYet($urlsArray) {
+    public function hasCrawlingDoneYet($urlsArray)
+    {
         foreach ($urlsArray as $key) {
-            if ($key['done']=='no') return false;
+            if ($key['done']=='no') {
+                return false;
+            }
         }
+        
         return true;
     }
     
-    public function endsWith($string, $endString) {
+    public function endsWith($string, $endString)
+    {
         $len = strlen($endString);
         if ($len == 0) {
             return true;
         }
+        
         return (substr($string, -$len) === $endString);
     }
 
-    public function urlStartsWith($url, $start) {
+    public function urlStartsWith($url, $start)
+    {
         $url_finder = preg_match(';(^'.str_replace('/', '\/', $start).');', $url, $url_found );
         if (@$url_found[0]) {
             return true;
-        } else {
-            return false;
         }
+         
+        return false;
     }
     
-    public function printlog($string, $title = null) {
+    public function printlog($string, $title = null)
+    {
         echo '/// '. (($title) ? $title : 'TITLE') .' ///' . PHP_EOL;
         echo $string;
         echo PHP_EOL . '/// ------------- ///' . PHP_EOL . PHP_EOL;
     }
 
-    public function dumplog($string, $title = null) {
+    public function dumplog($string, $title = null)
+    {
         echo '/// '. (($title) ? $title : 'TITLE') .' ///' . PHP_EOL;
         var_dump($string);
         echo PHP_EOL . '/// ------------- ///' . PHP_EOL . PHP_EOL;
     }
 
 }
-
-
-?>
